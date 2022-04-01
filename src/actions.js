00b09c3850c91d6f73220ed696aba8c8a9b3fe56
Samaelish <semnd007@gmail.com>
2022-04-01 14:32:07 +0300

    finished version

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+export const INCREASE = "INCREASE"
+export const DECREASE = "DECREASE"
+export const REMOVE = "REMOVE"
+export const CLEAR_CART = "CLEAR_CART"
+export const GET_TOTALS = "GET_TOTALS"
+export const TOGGLE_AMOUNT = "TOGGLE_AMOUNT"
+
+// action creator
+export const removeItem = id => {
+    return { type: REMOVE, payload: { id } }
+}
